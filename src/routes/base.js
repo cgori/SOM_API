e504853a400a9bebe8db78900d9f4ed8163bfaca
cgori <99cgor@gmail.com>
2020-04-24 15:20:59 +0100

    system status

--- src/routes/base.js
+++ src/routes/base.js
@@ -7,5 +7,6 @@ const router = express.Router();
 router.use('/auth', require('./auth'));
 router.use('/user', authMiddleware.protected, require('./user'));
 router.use('/DHT', authMiddleware.protected, require('./dht'));
+router.use('/status', authMiddleware.protected, require('./status'));
 
 module.exports = router;
